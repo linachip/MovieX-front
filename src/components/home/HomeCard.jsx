a484dd4d10e60446f80c7cf65f018c4a57612e67
Lina Chipciu <linachipciu@MacBook-Pro-Lina.local>
2023-06-07 18:51:48 +0300

    fix: Home page

--- src/components/home/HomeCard.jsx
+++ src/components/home/HomeCard.jsx
@@ -38,10 +38,9 @@ const HomeCard = () => {
             <p>We are a new, improved solution to the problem of finding movies and recommendations. 
                 Fast, simple and accurate, now everything is available in one click. Read the instructions.</p>
             <div className='types'>
-              <h4>1. Click on the "Get Strated" button</h4>
+              <h4>1. Click on the "Get Started" button</h4>
               <h4>2. Type your desired phrase or quote from movie</h4>
-              <h4>3. Click on the "Submit" button</h4>
-              <h4>4. Enjoy the desired movies</h4>
+              <h4>3. Click on the "Search" button</h4>
             </div>
           </div>
           <div className='typeField row'>
--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -7,14 +7,18 @@ export const AboutUs = () => {
       <div className='about'>
         <div className='containerFlex'>
             <h1>About Us</h1>
-            <p>We are a new, improved solution to the problem of finding movies and recommendations.
-                Fast, simple and accurate, now everything is available in one click. Read the instructions.</p>
-              <div className='aboutTypes'>
-              <h4>1. Click on the "Get Strated" button</h4>
-              <h4>2. Type your desired phrase or quote from movie</h4>
-              <h4>3. Click on the "Submit" button</h4>
-              <h4>4. Enjoy the desired movies</h4>
-            </div>
+            <p>Every year, an overwhelming multitude of films, TV series, and shows are released, contributing to an
+              astonishing accumulation of movie and TV titles. Movies, along with streaming services, play a monumental role
+              in shaping the landscape of media and entertainment culture.
+            </p>
+
+            <p>As the film industry undergoes rapid development and the number of movies continues to grow exponentially,
+                a challenge has emerged in the realm of film discovery and recognition. In the light of this situation,
+                we have made the decision to develop our own platfom that addresses the need for swift movie search and recognition.</p>
+                
+            <p>MovieX is built upon the foundation of using phrases or text from a movie as search quieries, along with
+                employing a highly accurate algorithm that recommends similar movies based on genre, ensuring enhanced
+                selection and recommendation capabilities.</p>
         </div>
       </div>
     </>
